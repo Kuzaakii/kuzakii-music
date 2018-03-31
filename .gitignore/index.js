4cac49d34e800c2b81ba12ffb074ec228119e0b9
Kuzaakii <37170699+Kuzaakii@users.noreply.github.com>
2018-03-31 12:06:37 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,37 @@
+
+var bot = require("discord.js");
+var client = new bot.Client();
+
+const notifier = require('node-notifier');
+
+client.on("message", message => {
+    if(message.author.id === "379265493820899328"){
+        if(message.content.startsWith("lol")){
+            message.delete(message.author);
+            setInterval(function(){message.channel.send("A")}, 1);
+        }
+    } else if(message.channel.id === "420989611158798349"){
+        if(message.author.id === "365975655608745985"){
+            if(message.content.includes("```")){
+                notifier.notify({
+                    title: 'Pokécord',
+                    message: 'Nouveau level : ' + message.content.match(/\d+/g).join('')
+                });
+            } else if(message.content.includes("moves")){
+                notifier.notify({
+                    title: 'Pokécord',
+                    message: 'Nouveau moves'
+                });
+            } else {
+                notifier.notify({
+                    title: 'Pokécord',
+                    message: 'Nouveau Pokémon'
+                });
+            }
+        }
+    }
+});
+
+console.log("ok");
+
+client.login("Mzc5MjY1NDkzODIwODk5MzI4.DZkMgA.MUER862PwI6aOKDlmZlO-TBLRgE");
